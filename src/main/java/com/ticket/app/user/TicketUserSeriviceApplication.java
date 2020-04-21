88d9d187e063995a0cee9ff2f11840372ce81f78
jose <josealves_w@yahoo.com.br>
2020-04-20 22:35:50 -0300

    configuration to work with spring eureka

--- src/main/java/com/ticket/app/user/TicketUserSeriviceApplication.java
+++ src/main/java/com/ticket/app/user/TicketUserSeriviceApplication.java
@@ -2,8 +2,10 @@ package com.ticket.app.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TicketUserSeriviceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ticket/app/user/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.ticket.app.user.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	private Environment env;
+	
+	
+	@GetMapping("/status/check")
+	public String status() {
+		return "working on port " + env.getProperty("local.server.port")+ ", with token = " +env.getProperty("token.secret");
+	}
+
+}
