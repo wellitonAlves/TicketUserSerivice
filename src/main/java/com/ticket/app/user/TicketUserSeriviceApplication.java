574c6bfc73ed4d4fa120651d92ed54c7249ac044
jose <josealves_w@yahoo.com.br>
2020-04-20 22:12:11 -0300

    first commit

---
+++ src/main/java/com/ticket/app/user/TicketUserSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.app.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketUserSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketUserSeriviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/app/user/TicketUserSeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.app.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketUserSeriviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
